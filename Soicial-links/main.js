3631075f736b1134bc7f1c8eaee7e3c829d15480
mr.drik <83313133+armandrik@users.noreply.github.com>
2021-08-18 21:41:36 +0430

    Add files via upload

---
+++ Soicial-links/main.js
@@ -0,0 +1,16 @@
+const instagram = document.getElementById('instagram');
+instagram.addEventListener("click", function() {
+    window.open("https://www.instagram.com/arman.drik/");
+})
+const github = document.getElementById('github');
+github.addEventListener("click", function() {
+    window.open("https://github.com/armandrik");
+})
+const twitter = document.getElementById('twitter');
+twitter.addEventListener("click", function() {
+    window.open("https://twitter.com/Armandriik?s=09");
+})
+const linkdin = document.getElementById('linkdin');
+linkdin.addEventListener("click", function() {
+    window.open("https://www.linkedin.com/in/arman-drik-930460217/");
+})
